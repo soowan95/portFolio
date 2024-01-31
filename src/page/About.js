6c7674b14e49b84820e930615875e97e6248c5ea
soowan95 <ksw9565@gmail.com>
2024-01-31 14:08:46 +0900

    모바일버전 글자 크기 수정

--- src/page/About.js
+++ src/page/About.js
@@ -67,8 +67,8 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             7공수 특전여단 장교 출신으로 2년간의 팀 생활을 통해 내가 소속된
-            집단의 발전을 <br /> 위해 노력해오며 내가 집단을 위해 할 일이
-            무엇인지 많은 생각을 했습니다.
+            집단의 발전을 위해 노력해오며 내가 집단을 위해 할 일이 무엇인지 많은
+            생각을 했습니다.
           </Box>
         </Flex>
         <Flex pl={"5px"} mt={"10px"}>
@@ -77,7 +77,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             외부 훈련이 많았던 부대인만큼 훈련 전에 팀원들과의 소통과 협력을
-            통해 임무를 <br /> 완수하는 것이 핵심이었습니다.
+            통해 임무를 완수하는 것이 핵심이었습니다.
           </Box>
         </Flex>
         <Flex pl={"5px"} mt={"10px"}>
@@ -86,7 +86,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             군 생활의 이러한 경험은 팀 프로젝트를 진행하면서 팀원들의 다양한
-            의견을 <br /> 수용하고 효과적으로 팀 혐업을 이끌어내는데 큰 도움이
+            의견을 수용하고 효과적으로 팀 혐업을 이끌어내는데 큰 도움이
             됐습니다.
           </Box>
         </Flex>
@@ -99,7 +99,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             이전 직장에서의 힘들었던 시기는 저에게 적응력과 팀워크를 키우는 좋은
-            계기가 <br /> 됐습니다.
+            계기가 됐습니다.
           </Box>
         </Flex>
         <Flex pl={"5px"} mt={"10px"}>
@@ -108,7 +108,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             힘들었던 시기를 극복하고자 매주 월요일 오전에 팀원끼리 모여 공부한
-            내용들을 <br /> 공유하고 각자의 노하우를 배우며 노력했습니다.
+            내용들을 공유하고 각자의 노하우를 배우며 노력했습니다.
           </Box>
         </Flex>
         <Flex pl={"5px"} mt={"10px"}>
@@ -117,8 +117,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             이렇듯 저는 당장의 어려움에 주저앉기보단 조직을 바라보며 팀원들과의
-            충분한 <br /> 소통과 협력을 통해 제가 목표한 바를 이뤄가는
-            사람입니다.
+            충분한 소통과 협력을 통해 제가 목표한 바를 이뤄가는 사람입니다.
           </Box>
         </Flex>
       </Collapse>
@@ -164,7 +163,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             군생활 중에는 팀원의 사고로 징계를 받았고, 전역 후 트레이너 생활
-            때는 코로나로 <br /> 인해 타격이 컸습니다.
+            때는 코로나로 인해 타격이 컸습니다.
           </Box>
         </Flex>
         <Flex pl={"5px"} mt={"10px"}>
@@ -173,7 +172,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             그때마다 잘 될 거라는 생각과 그 생각들을 실현하고자 하는 노력으로
-            극복해 <br /> 왔습니다.
+            극복해 왔습니다.
           </Box>
         </Flex>
         <Flex pl={"5px"} mt={"10px"}>
@@ -182,8 +181,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             팀 프로젝트를 진행하면서도 예기치 못한 오류들로 어려움이 있었지만,
-            이전의 <br /> 극복했던 경험들로 포기하지 않고 끝까지 해낼 수
-            있었습니다.
+            이전의 극복했던 경험들로 포기하지 않고 끝까지 해낼 수 있었습니다.
           </Box>
         </Flex>
         <Box pl={"5px"} fontWeight={"bolder"} mt={"30px"}>
@@ -195,8 +193,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             제가 맡은 일에는 큰 책임감을 느끼기에 저의 일을 반드시
-            마무리하겠다는 신념이 <br /> 있지만 때로는 부담으로 다가올 때도
-            있었습니다.
+            마무리하겠다는 신념이 있지만 때로는 부담으로 다가올 때도 있었습니다.
           </Box>
         </Flex>
         <Flex pl={"5px"} mt={"10px"}>
@@ -205,7 +202,6 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             팀 프로젝트 중 WebSocket을 사용하는 일을 담당했는데 처음 사용해보는
-            <br />
             프로토콜이라 구현하는데 있어 어려움을 겪었습니다.
           </Box>
         </Flex>
@@ -215,8 +211,8 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             저의 업무였기에 혼자 해결하기 위해 많은 시간과 노력을 소비하던 중
-            팀원의 <br /> 제안으로 stompjs를 사용하여 해결하며 팀원들과 의견을
-            공유하고 함께 해결하는 <br /> 것의 중요함을 배웠습니다.
+            팀원의 제안으로 stompjs를 사용하여 해결하며 팀원들과 의견을 공유하고
+            함께 해결하는 것의 중요함을 배웠습니다.
           </Box>
         </Flex>
         <Flex pl={"5px"} mt={"10px"}>
@@ -225,7 +221,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             여전히 저에게 주어진 일에 막중한 책임감을 느끼지만 어려움이 있을 때
-            팀원들과의 <br /> 충분한 소통을 통해 극복해 나아가고 있습니다.
+            팀원들과의 충분한 소통을 통해 극복해 나아가고 있습니다.
           </Box>
         </Flex>
       </Collapse>
@@ -271,7 +267,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             저의 업무 이외에도 회사의 여러 직무에 유연하게 적응하여 뛰어난
-            팀원으로서 <br /> 기여하고자 합니다.
+            팀원으로서 기여하고자 합니다.
           </Box>
         </Flex>
         <Flex pl={"5px"} mt={"10px"}>
@@ -280,7 +276,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             전 직장에서 근무 중 개발에 대한 지식은 없었지만 여러 문서들의
-            전산화를 위해 <br /> 구글 스프레드 시트와 App script를 공부하여
+            전산화를 위해 구글 스프레드 시트와 App script를 공부하여
             해결했습니다.
           </Box>
         </Flex>
@@ -290,7 +286,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             지속적인 자기 개발을 통해 회사의 성장과 발전에 기여할 것이며, 새로운
-            기술을 <br /> 습득하는데 있어 항상 열린 마음으로 임하겠습니다.
+            기술을 습득하는데 있어 항상 열린 마음으로 임하겠습니다.
           </Box>
         </Flex>
         <Box pl={"5px"} fontWeight={"bolder"} mt={"30px"}>
@@ -302,7 +298,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             학원에서의 경험을 통해 끊임없는 학습과 개발에 대한 열정으로 새로운
-            기술을 <br /> 빠르게 습득하여 프로젝트에 적응할 수 있습니다.
+            기술을 빠르게 습득하여 프로젝트에 적응할 수 있습니다.
           </Box>
         </Flex>
         <Flex pl={"5px"} mt={"10px"}>
@@ -311,8 +307,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             mybatis의 쿼리문을 반복해서 작성해야 한다는 불편함을 줄이고자 jpa를
-            학습했고, <br /> 보안의 편리한 구현을 위해 spring security를
-            학습했습니다.
+            학습했고, 보안의 편리한 구현을 위해 spring security를 학습했습니다.
           </Box>
         </Flex>
         <Flex pl={"5px"} mt={"10px"}>
@@ -321,7 +316,6 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             보다 실무적인 협업 환경을 구성하기 위해 swagger를 도입하여 팀원들과
-            <br />
             소통했습니다.
           </Box>
         </Flex>
@@ -331,7 +325,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             이러한 노력을 통해 회사에서 주어진 업무 외의 부분에서도 적극적으로
-            도전해 <br /> 나아가겠습니다.
+            도전해 나아가겠습니다.
           </Box>
         </Flex>
         <Flex pl={"5px"} mt={"10px"}>
@@ -340,7 +334,7 @@ function About() {
           </Box>
           <Box className={"about-body-content-text"}>
             회사의 비전과 목표에 부합하는 프로젝트에 주도적으로 참여하여 회사의
-            핵심 <br /> 가치를 실현해 나아가겠습니다.
+            핵심 가치를 실현해 나아가겠습니다.
           </Box>
         </Flex>
       </Collapse>
