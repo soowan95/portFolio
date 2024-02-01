b9904e23787e98dcc03766172a6c13e1b5b49bf7
soowan95 <ksw9565@gmail.com>
2024-02-01 12:47:43 +0900

    경력 작성 중...

--- src/util/CareerContent.js
+++ src/util/CareerContent.js
@@ -4,8 +4,8 @@ import { Box, Flex } from "@chakra-ui/react";
 function CareerContent({ duration, content }) {
   return (
     <motion.div
-      initial={{ opacity: 0, x: 50 }}
-      whileInView={{ opacity: 1, x: 0 }}
+      initial={{ opacity: 0, x: 50, y: -20 }}
+      whileInView={{ opacity: 1, x: 0, y: 0 }}
       transition={{ duration: 2 }}
       className={"career-header-box-content"}
     >
